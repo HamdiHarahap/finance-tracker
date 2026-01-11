2919918887905d47a4da3b39938b7a25078dfaa4
HamdiHarahap <hamdiharahap2005@gmail.com>
2026-01-11 17:47:23 +0700

    Update Balance component to display formatted saldo, pemasukan, and pengeluaran totals

--- src/ui/components/fragments/Balance.jsx
+++ src/ui/components/fragments/Balance.jsx
@@ -5,6 +5,19 @@ import { useState } from 'react';
 const Balance = () => {
 	const [showForm, setShowForm] = useState(false);
 
+	const data = JSON.parse(localStorage.getItem('user'));
+
+	const saldo = data.balance;
+	const transactions = data.transactions || [];
+
+	const pemasukanTotal = transactions
+		.filter((item) => item.jenis == 'pemasukan')
+		.reduce((total, item) => total + item.jumlah, 0);
+
+	const pengeluaranTotal = transactions
+		.filter((item) => item.jenis == 'pengeluaran')
+		.reduce((total, item) => total + item.jumlah, 0);
+
 	const handleShowForm = () => {
 		setShowForm(!showForm);
 	};
@@ -15,21 +28,38 @@ const Balance = () => {
 				<div className="flex justify-between items-center">
 					<div>
 						<h1 className="text-sm text-gray-500">Total Saldo</h1>
-						<p className="text-2xl font-bold text-slate-800">Rp 300.000</p>
+						<p className="text-2xl font-bold text-slate-800">
+							{saldo.toLocaleString('id-ID', {
+								style: 'currency',
+								currency: 'IDR',
+								minimumFractionDigits: 0,
+							})}
+						</p>
 					</div>
 
 					<Button onClick={handleShowForm}>Tambah Transaksi</Button>
 				</div>
 
-				<div className="grid grid-cols-2 gap-4">
+				<div className="grid grid-cols-2 gap-4 max-[520px]:grid-cols-1">
 					<div className="bg-slate-50 border border-slate-200 rounded-xl p-4">
 						<h2 className="text-sm text-gray-500">Total Pengeluaran</h2>
-						<p className="text-2xl font-bold text-red-500">Rp 1.000.000</p>
+						<p className="text-2xl font-bold text-red-500">
+							{pengeluaranTotal.toLocaleString('id-ID', {
+								style: 'currency',
+								currency: 'IDR',
+								minimumFractionDigits: 0,
+							})}
+						</p>
 					</div>
-
 					<div className="bg-slate-50 border border-slate-200 rounded-xl p-4">
 						<h2 className="text-sm text-gray-500">Total Pemasukan</h2>
-						<p className="text-2xl font-bold text-green-600">Rp 1.300.000</p>
+						<p className="text-2xl max-[520px]:text-xl font-bold text-green-600">
+							{pemasukanTotal.toLocaleString('id-ID', {
+								style: 'currency',
+								currency: 'IDR',
+								minimumFractionDigits: 0,
+							})}
+						</p>
 					</div>
 				</div>
 			</div>
@@ -40,7 +70,7 @@ const Balance = () => {
 						className="absolute inset-0 bg-black/50 backdrop-blur-sm"
 						onClick={handleShowForm}
 					/>
-					<FormTransaction />
+					<FormTransaction onClick={handleShowForm} />
 				</div>
 			)}
 		</>
